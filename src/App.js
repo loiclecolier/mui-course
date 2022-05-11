2636eebc603de9b81b9205fa70314e0a79131297
Loïc Lécolier <loic.lecolier@outlook.be>
2022-05-11 18:51:56 +0200

    MUI Layouts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Box, Stack } from "@mui/material";
+import { Feed } from "./components/Feed"
+import { Rightbar } from "./components/Rightbar"
+import { Sidebar } from "./components/Sidebar"
+
 function App() {
   return (
-    <div>
-      Hello
-    </div>
-  );
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+export const Rightbar = () => {
+  return (
+    <Box
+        bgcolor="lightcoral"
+        flex={2}
+        p={2}>
+            Rightbar
+    </Box>
+  )
+}
